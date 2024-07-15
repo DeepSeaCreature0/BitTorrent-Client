e0876a9cb85ce2d0fc6a9468e5c84b65110243d7
Gaurav <gauravmahaur2@gmail.com>
2024-07-15 12:54:18 +0530

    added connection request and response function

--- tracker.js
+++ tracker.js
@@ -3,33 +3,34 @@
 const dgram = require('dgram');
 const Buffer = require('buffer').Buffer;
 const urlParse = require('url').parse;
+const crypto = require('crypto');
 
 module.exports.getPeers = (torrent, callback) => {
-  const socket = dgram.createSocket('udp4');
-  const url = torrent.announce.toString('utf8');
+    const socket = dgram.createSocket('udp4');
+    const url = torrent.announce.toString('utf8');
 
-  // 1. send connect request
-  udpSend(socket, buildConnReq(), url);
+    // 1. send connect request
+    udpSend(socket, buildConnReq(), url);
 
-  socket.on('message', response => {
+    socket.on('message', response => {
     if (respType(response) === 'connect') {
-      // 2. receive and parse connect response
-      const connResp = parseConnResp(response);
-      // 3. send announce request
-      const announceReq = buildAnnounceReq(connResp.connectionId);
-      udpSend(socket, announceReq, url);
+        // 2. receive and parse connect response
+        const connResp = parseConnResp(response);
+        // 3. send announce request
+        const announceReq = buildAnnounceReq(connResp.connectionId);
+        udpSend(socket, announceReq, url);
     } else if (respType(response) === 'announce') {
-      // 4. parse announce response
-      const announceResp = parseAnnounceResp(response);
-      // 5. pass peers to callback
-      callback(announceResp.peers);
+        // 4. parse announce response
+        const announceResp = parseAnnounceResp(response);
+        // 5. pass peers to callback
+        callback(announceResp.peers);
     }
   });
 };
 
 function udpSend(socket, message, rawUrl, callback=()=>{}) {
-  const url = urlParse(rawUrl);
-  socket.send(message, 0, message.length, url.port, url.hostname, callback);
+    const url = urlParse(rawUrl);
+    socket.send(message, 0, message.length, url.port, url.hostname, callback);
 }
 
 function respType(resp) {
@@ -37,11 +38,25 @@ function respType(resp) {
 }
 
 function buildConnReq() {
-  // ...
+    const buf = Buffer.allocUnsafe(16);
+
+    // connection id
+    buf.writeUInt32BE(0x417, 0);
+    buf.writeUInt32BE(0x27101980, 4);
+    // action
+    buf.writeUInt32BE(0, 8);
+    // transaction id
+    crypto.randomBytes(4).copy(buf, 12);
+
+    return buf;
 }
 
 function parseConnResp(resp) {
-  // ...
+    return {
+        action: resp.readUInt32BE(0),
+        transactionId: resp.readUInt32BE(4),
+        connectionId: resp.slice(8)
+    }
 }
 
 function buildAnnounceReq(connId) {
