5e7c39aedbf749d88a75fed78cc0b22becb9437b
Gaurav <gauravmahaur2@gmail.com>
2024-07-15 13:32:00 +0530

    added argument functionality in index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 'use strict';
 
 const tracker = require('./tracker');
-
 const torrentParser = require('./torrent-parser');
 
-const torrent = torrentParser.open('puppy.torrent');
+const torrentFileName = process.argv[2]+".torrent";
+
+if (!torrentFileName) {
+    console.error('Please provide a torrent file name as an argument.');
+    process.exit(1);
+}
+
+const torrent = torrentParser.open(torrentFileName);
 
 tracker.getPeers(torrent, peers => {
     console.log('list of peers: ', peers);
-  });
+});
