a1db1d2ae73c5ab8f8d1b895dcd71e27e630b0fe
DeepSeaCreature0 <gauravmahaur2@gmail.com>
2024-07-16 14:54:02 +0530

    added onWholeMsg

--- src/download.js
+++ src/download.js
@@ -8,20 +8,37 @@ const tracker =require('./tracker'); // all function related to tracker like con
 
 const main= torrent=>{
     tracker.getPeers(torrent, peers => {
-        peers.forEach(downloadFromPeer);
+        peers.forEach(handlePeer);
     });
 }
 
-function downloadFromPeer(peer){
-    const socket =net.Socket();
+function handlePeer(peer){
+    const socket = net.Socket();
     socket.on('error',console.log);
     socket.connect(peer.port,peer.ip,()=>{
         // message
     });
-    socket.on('data',data=>{
-        // handle response
+    onWholeMsg(socket, data => {
+        // handle response here
     });
 };
+
+function onWholeMsg(socket, callback) {
+    let savedBuf = Buffer.alloc(0);
+    let handshake = true;
+    
+    socket.on('data', recvBuf => {
+      // msgLen calculates the length of a whole message
+      const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+      savedBuf = Buffer.concat([savedBuf, recvBuf]);
+  
+      while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+        callback(savedBuf.slice(0, msgLen()));
+        savedBuf = savedBuf.slice(msgLen());
+        handshake = false;
+      }
+    });
+  }
 
 module.exports={
     main,
